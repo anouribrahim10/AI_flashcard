92fbb980118a0e46c7e6303b7ae9be31c93821ee
Shafin-Rehman <shafinammin@gmail.com>
2024-08-18 22:28:09 -0400

    Hi

--- app/page.js
+++ app/page.js
@@ -1,11 +1,32 @@
 import Image from "next/image";
 import getStripe from "@/utils/get-stripe";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { Button, Container, Typography } from "@mui/material";
+import Head from "next/head";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
 
 
 export default function Home() {
   return (
+    <Container maxWidth="lg">
+      <Head>
+        <title>Flashcard SaaS</title>
+        <meta name="description" content="Create flashcards from your text" />
+      </Head>
 
-  )
-
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">Flashcard SaaS</Typography>
+          <SignedOut>
+            <Button>Login</Button>
+            <Button>Sign up</Button>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </Toolbar>
+      </AppBar>
+    </Container>
+  );
 }
